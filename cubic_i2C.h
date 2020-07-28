5f81c01c0c41a376e4c1d9e64313f116356d3ef8
Shaogang Zhang <Shaogang.Zhang@flex.com>
2020-07-28 15:37:44 -0400

    added h file

---
+++ cubic_i2C.h
@@ -0,0 +1,12 @@
+/*******************************************************************************
+*                                 Prototypes                                   *
+*******************************************************************************/
+
+/************ I2C Interface *******************************/
+void I2C_Read (unsigned char, unsigned char , unsigned char , unsigned char* );
+unsigned int I2C_Read_Byte (unsigned char, unsigned char);
+void I2C_Write (unsigned char, unsigned char, unsigned char, unsigned char[],unsigned char);
+void I2C_Write_Byte (unsigned char, unsigned char, unsigned char);
+unsigned int I2C_Read_NOREG ( unsigned char Slave_Address);
+
+
